6b79cde1142d4c65e16c8a3910480201dd4da577
Techie-Bee <89488854+Techie-Bee@users.noreply.github.com>
2021-10-21 03:20:04 +0530

    Add files via upload

---
+++ R/ds.R
@@ -0,0 +1,14 @@
+#' @export
+#' @param x numeric variable
+ds <- function(x)
+{
+  par(mfrow = c(1,2))
+  #Histogram
+  hist(x, col = rainbow(30))
+  #Boxplot
+  boxplot(x, col = 'green')
+  par(mfrow = c(1,1))
+  #Numeric summary
+  data.frame(min=min(x),
+             median = median(x))
+}
