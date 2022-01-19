c539339deafc2c2ee6b5a4de479c3fb408780a12
Techie-Bee <89488854+Techie-Bee@users.noreply.github.com>
2022-01-19 21:08:43 +0530

    Add files via upload

--- R/ds.R
+++ R/ds.R
@@ -2,16 +2,16 @@
 #' @param x numeric variable
 ds <- function(x)
 {
-  par(mfrow = c(1,2))
+  par(mfrow = c(1,3))
   #Histogram
   hist(x, col = rainbow(30))
   #Boxplot
   boxplot(x, col = 'green')
-  par(mfrow = c(1,1))
+  par(mfrow = c(1,2))
   #Barplot
   barplot(x)
+  par(mfrow = c(1,1))
   #Numeric summary
   data.frame(min=min(x),
              median = median(x))
-showProgress=getOption("datatable.showProgress", interactive())		
 }
