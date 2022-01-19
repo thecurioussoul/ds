7d61cd9f434e58a526030be798377427f811cfce
Techie-Bee <89488854+Techie-Bee@users.noreply.github.com>
2022-01-19 21:12:22 +0530

    Add files via upload

--- R/ds.R
+++ R/ds.R
@@ -2,14 +2,11 @@
 #' @param x numeric variable
 ds <- function(x)
 {
-  par(mfrow = c(1,3))
+  par(mfrow = c(1,2))
   #Histogram
   hist(x, col = rainbow(30))
   #Boxplot
   boxplot(x, col = 'green')
-  par(mfrow = c(1,2))
-  #Barplot
-  barplot(x)
   par(mfrow = c(1,1))
   #Numeric summary
   data.frame(min=min(x),
